794dc8016ec7606d94da89f807d5ab6832959f7c
im-azad <me.azad99@gmail.com>
2023-04-28 10:55:41 +0600

    count 2 solved

---
+++ Count_II.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    char str[1001];
+    scanf("%s", str);
+    int count = 0;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+        {
+            count++;
+        }
+    }
+    printf("%d", count);
+
+    return 0;
+}
