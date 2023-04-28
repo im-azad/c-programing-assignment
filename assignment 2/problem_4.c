bd084d6d3756b60cb954ea750b55b2f6626c6a8f
im-azad <me.azad99@gmail.com>
2023-04-28 09:53:08 +0600

    conut 1 problem solve

---
+++ assignment 2/problem_4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int x, v;
+    scanf("%d %d", &x, &v);
+    for (int i = 0; i < n; i++)
+    {
+        arr[x] = v;
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
---
+++ mid_term_exam/count1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int size;
+    scanf("%d", &size);
+    int arr[size], even_count = 0, odd_count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            even_count++;
+        }
+        else
+        {
+            odd_count++;
+        }
+    }
+    printf("%d %d", even_count, odd_count);
+
+    return 0;
+}
